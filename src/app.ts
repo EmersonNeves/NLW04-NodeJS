ff9b79983b2669f0c7a71473fdabd1ff67adeeab
EmersonNeves <emersonneves@outlook.com>
2021-02-28 20:55:10 -0300

    first commit

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+
 app.use(
   (err: Error, request: Request, response: Response, _next: NextFunction) => {
     if (err instanceof AppErrors) {
